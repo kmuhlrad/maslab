884303586d76f0e68a55f9aff8163fffb55b16fc
Katy Muhlrad <kmuhlrad@gmail.com>
2015-01-07 16:13:51 -0500

    Changed test code to driving

--- test/motor.cpp
+++ test/motor.cpp
@@ -76,15 +76,15 @@ int main() {
   while (running) {
     //std::cout << "Speed: " << "0.5" << std::endl;
     setMotorSpeed(right_motor, right_dir, -0.1);
-    setMotorSpeed(left_motor, left_motor, 0.1);
+    setMotorSpeed(left_motor, left_dir, 0.1);
     sleep(1.0);
 
     setMotorSpeed(right_motor, right_dir, 0.0);
-    setMotorSpeed(left_motor, left_motor, 0.0);
+    setMotorSpeed(left_motor, left_dir, 0.0);
     sleep(1.0);
 
     setMotorSpeed(right_motor, right_dir, 0.1);
-    setMotorSpeed(left_motor, left_motor, -0.1);
+    setMotorSpeed(left_motor, left_dir, -0.1);
     sleep(1.0);
     /*speed += 0.1;
     if (speed > 1.0) {
