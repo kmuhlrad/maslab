3be2ed5fa8c7fdcf8f2ac62110064300730cf94e
Katy Muhlrad <kmuhlrad@gmail.com>
2015-01-10 18:44:06 +0000

    fixed errors

--- test/dio.cpp
+++ test/dio.cpp
@@ -9,13 +9,14 @@
 
 int running = 1;
 
+/*
 void on(mraa::Gpio pin) {
   pin.write(1);
 }
 
 void off(mraa::Gpio pin) {
   pin.write(0);
-}
+}*/
 
 void sig_handler(int signo)
 {
@@ -33,26 +34,27 @@ int main() {
   mraa::Gpio gpio = mraa::Gpio(13);
   gpio.dir(mraa::DIR_OUT);
 
-  //short range IR sensor
+  // short range IR sensor
   mraa::Gpio sr_ir = mraa::Gpio(12);
   sr_ir.dir(mraa::DIR_IN);
 
   //testing code
   while (running) {
     //this should do the same thing as below
+    /*
     on(gpio);
     sleep(1);
     off(gpio);
-    sleep(1);
+    sleep(1);*/
+
 
     std::cout << "IR sensor: " << sr_ir.read() << std::endl;
     //old code - eventually delete
-    /*printf("Gpio high\n");
+    printf("Gpio high\n");
     gpio.write(1);
     sleep(1);
     printf("Gpio low\n");
     gpio.write(0);
     sleep(1);
-    */
   }
 }
