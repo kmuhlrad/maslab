ae55d94d72e2126996f672584216c652cfa8b505
Katy Muhlrad <kmuhlrad@gmail.com>
2015-01-13 11:47:37 -0500

    updated motor class

--- test/hardware/motor.cpp
+++ test/hardware/motor.cpp
@@ -11,12 +11,15 @@
 #include "motor.h"
 #include "mraa.hpp"
 
+mraa::Pwm pwm;
+mraa::Gpio dir;
+
 Motor::Motor(int pwm_pin, int dir_pin) {
-  mraa::Pwm& pwm = mraa::Pwm(pwm_pin);
+  pwm = mraa::Pwm(pwm_pin);
   pwm.write(0.0);
   pwm.enable(true);
 
-  mraa::Gpio& dir = mraa::Gpio(dir_pin);
+  dir = mraa::Gpio(dir_pin);
   dir.dir(mraa::DIR_OUT);
   dir.write(0);
 }
