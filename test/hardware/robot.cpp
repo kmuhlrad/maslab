3de6956265277621ca61f3cf4929a568f9a9b10d
Katy Muhlrad <kmuhlrad@gmail.com>
2015-01-14 16:56:00 -0500

    changed output

--- test/hardware/robot.cpp
+++ test/hardware/robot.cpp
@@ -137,9 +137,9 @@ int main() {
     //desired should come from external input: cube location or something
     //estimated should come from the current gryo angle reading
     //speed should depend on external input from distance sensors or camera
-    current_ang = gyro.get_angle() - init_ang;
+    current_ang = gyro.get_angle();
     //drive_straight(left, right, gyro, -10.0, current_ang, 0.3);
-    std::cout << "init: " << init_ang << std::endl;
+    //std::cout << "init: " << init_ang << std::endl;
     std::cout << "cur: " << current_ang << std::endl;
   }
 
