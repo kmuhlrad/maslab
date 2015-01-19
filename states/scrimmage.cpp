bcb6d496cb048ffdca519ef96d3226a4edc972f1
Katy Muhlrad <kmuhlrad@gmail.com>
2015-01-19 15:23:49 -0500

    fixed function name errors

--- states/scrimmage.cpp
+++ states/scrimmage.cpp
@@ -79,8 +79,8 @@ void drive_straight(Motor& left, Motor& right, Gyro& gyro,
   float derivative = diff - last_diff;
   float power = P*diff + I*integral + D*derivative;
 
-  left.setSpeed(speed + power);
-  right.setSpeed(-(speed - power));
+  left.setSpeed((speed + power), 0.3);
+  right.setSpeed(-(speed - power), 0.3);
 
   gettimeofday(&start, NULL);
   last_diff = diff;
