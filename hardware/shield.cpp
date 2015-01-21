ed87f8fdaa7b7ebed13b0b506e31a6bcbe3790db
Katy Muhlrad <kmuhlrad@gmail.com>
2015-01-21 16:09:36 -0500

    made writebuff unsigned

--- hardware/shield.cpp
+++ hardware/shield.cpp
@@ -31,7 +31,7 @@ Shield::Shield() {
 }
 
 void Shield::initPWM(mraa::I2c *i2c) {
-  char writeBuf[2] = {0};
+  unsigned char writeBuf[2] = {0};
   writeBuf[0] = 0x00; // Write to MODE 1 Register;
   writeBuf[1] = 1 << 4; // Enable Sleep Mode
  
@@ -60,7 +60,7 @@ void Shield::writePWM(mraa::I2c* i2c, int index, double duty) {
   double on = 4095.0 * duty;
   uint16_t onRounded = (uint16_t) on;
 
-  char writeBuf[5];
+  unsigned char writeBuf[5];
 
   // ON_L
   writeBuf[0] = registers[index];
